c306d47b5124522e41dd919e1011ef1d1f73189e
Kracklitos <kracklitos1@gmail.com>
2024-07-21 13:39:46 -0400

    Script del personaje con las colisiones y para dibujar en el canvas el sprite

--- adventure/character.py
+++ adventure/character.py
@@ -97,3 +97,87 @@ class Character:
         self.x += dvx 
         # Actualiza la posición y del personaje
         self.y += (self.vy * delay)
+
+    # Dibuja el personaje en el canvas
+    def draw(self, canvas):
+        r = self.last_test == LEFT_BLOCK  # Indica si el personaje está mirando a la izquierda
+        # Dibuja el sprite del personaje según su estado
+        if self.status.name == "idle" or self.status.name == "run" or self.status.name == "djump":
+            i = self.status.index % self.status.count  # Obtiene el índice del sprite actual
+            p = adventure.default.texture.get_texture(self.sprite, r)  # Obtiene la textura del sprite
+            s = p.subsurface((i * self.w, 0, self.w, self.h))  # Extrae el sub-rectángulo del sprite
+        else:
+            s = adventure.default.texture.get_texture(self.sprite, r)  # Obtiene la textura del sprite
+        canvas.blit(s, (self.x, self.y))  # Dibuja el sprite en el canvas
+        # Dibuja el bloque objetivo (para debug)
+        if self.target is not None:
+            x, y = self.target
+            pygame.draw.rect(adventure.default.canvas, (100, 0, 0), (x * 32, y * 32, 32, 32))
+
+    # Obtiene el rectángulo del bloque actual que ocupa el personaje
+    def get_current_block(self):
+        block_size = adventure.default.block_size  # Obtiene el tamaño del bloque
+        x = self.x / block_size  # Calcula la coordenadas
+        y = self.y / block_size  
+        c_w = self.x + self.w  # Calcula la coordenada x del borde derecho del personaje
+        c_h = self.y + self.h  # Calcula la coordenada y del borde inferior del personaje
+        c_w1 = c_w / block_size - x  # Calcula el ancho del bloque en términos de bloques
+        c_h1 = c_h / block_size - y  # Calcula el alto del bloque en términos de bloques
+        # Ajusta el ancho y alto para que incluyan los bloques parciales
+        w = c_w1 + 1 if ((c_w % block_size) > 0.5) else c_w1 
+        h = c_h1 + 1 if ((c_h % block_size) > 0.5) else c_h1 
+        return pygame.Rect(x, y, w, h)  # Devuelve el rectángulo del bloque
+
+    # Obtiene los bloques circundantes al bloque actual del personaje
+    def get_surrounding_block(self, test):
+        r = self.get_current_block()  # Obtiene el rectángulo del bloque actual
+        test_w = range(-1, r.w * 2)  # Genera un rango para el ancho del bloque
+        test_h = range(-1, r.h * 2)  # Genera un rango para el alto del bloque
+        result = []  # Inicializa la lista de bloques circundantes
+        # Itera sobre los bloques circundantes
+        for offset_y in range(0, r.h * 2 + r.h % 2):
+            pos_y = r.y + test_h[offset_y]  
+            for offset_x in range(0, r.w * 2 + r.w % 2):
+                pos_x = r.x + test_w[offset_x]  
+                # Itera sobre la lista de direcciones de prueba
+                for t in test:
+                    mx, my = t  # Obtiene el desplazamiento x e y de la dirección
+                    # Si el punto de prueba está dentro del rectángulo del bloque
+                    if r.collidepoint(pos_x + mx, pos_y + my):
+                        result.append((pos_x, pos_y))  # Agrega el bloque a la lista de resultados
+        return result  # Devuelve la lista de bloques circundantes
+
+    # Actualiza el bloque objetivo del personaje (para detección de colisiones)
+    def update_target(self, test):
+        self.last_blocks = self.get_surrounding_block(test)  # Obtiene los bloques circundantes
+        adventure.default.draw_blocks()  # Dibuja los bloques (para debug)
+        self.target = None  # Inicializa el bloque objetivo
+        flag = False  # Bandera para indicar si se encontró un bloque objetivo
+        # Itera sobre los bloques circundantes
+        for block in self.last_blocks:
+            x, y = block  # Obtiene las coordenadas del bloque
+            b = adventure.default.get_block_id(x, y)  # Obtiene el ID del bloque
+            # Si el bloque no es nulo y no se encontró un bloque objetivo antes
+            if b is not None and flag is False:
+                self.target = block  # Asigna el bloque como objetivo
+                flag = True  # Establece la bandera como verdadera
+                x, y = block  # Obtiene las coordenadas del bloque
+                pygame.draw.rect(adventure.default.canvas, (0, 100, 0), (x * 32, y * 32, 32, 32))  # Dibuja el bloque objetivo
+
+    # Maneja las acciones al colisionar con un bloque
+    def on_collision(self, b_id):
+        block = adventure.default.blocks[b_id]  # Obtiene el bloque del diccionario de bloques
+        # Si el bloque tiene la propiedad "collision"
+        if "collision" in block:
+            # Si la propiedad "collision" es "died"
+            if block["collision"] == "died":
+                adventure.default.restart()  # Reinicia el juego
+            # Si la propiedad "collision" es "jump"
+            elif block["collision"] == "jump":
+                #self.status = StatusDJump()  # Cambia el estado a 'djump'
+                self.sprite = SPRITE_DJUMP  # Cambia el sprite a 'djump'
+                self.djump = False  # Deshabilita el doble salto
+                self.vy = -600  # Aplica la potencia del salto
+        # Si el bloque tiene la propiedad "transport"
+        if "transport" in block:
+            adventure.default.load_level(block["transport"])  # Carga el nivel indicado
